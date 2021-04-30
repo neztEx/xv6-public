98b38c954bfb3ced14602eee40ea5c6c93211fba
abdelrahim-hentabli <abdelrahim.hentabli@gmail.com>
2021-04-30 16:27:17 -0700

    timing set up

--- testLottery.c
+++ testLottery.c
@@ -10,19 +10,19 @@ int	main(int argc, char *argv[])
     int	i, k;
     const int loop = 43000; //43000
     int startTicks = tick();
-    // int currentTicks[loop / 1000];
+    int currentTicks[loop / 1000];
     for(i=0; i<loop; i++) {
         asm("nop"); //in order to prevent the compiler from optimizing the for loop   
         for(k=0;k<loop;k++) {
             asm("nop");
         }
-        // if(i % 1000 == 0)
-        //     currentTicks[i / 1000] = tick() - startTicks;
+        if(i % 1000 == 0)
+            currentTicks[i / 1000] = tick() - startTicks;
     }
     
-    // for(i = 0; i <43; i++){
-    //     printf(1,"Process %d: %d Tickets and %d ticks\n", v, n, currentTicks[i]);
-    // }
+    for(i = 0; i <43; i++){
+        printf(1,"Process %d: %d Tickets and %d ticks\n", v, n, currentTicks[i]);
+    }
     printf(1,"Process %d: %d Tickets and %d ticks\n", v, n, tick() - startTicks);
     
     exit();
